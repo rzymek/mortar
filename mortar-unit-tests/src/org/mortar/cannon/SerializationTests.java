ecc19c44c9f53bd6ee4286f373dcae557766d892
rzymek <rzymek@gmail.com>
2014-06-30 03:01:56 +0200

    instanceOf test

--- mortar-unit-tests/src/org/mortar/cannon/SerializationTests.java
+++ mortar-unit-tests/src/org/mortar/cannon/SerializationTests.java
@@ -1,10 +1,12 @@
 package org.mortar.cannon;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mortar.common.MessageSerializer;
@@ -12,6 +14,7 @@ import org.mortar.common.msg.ConfigMsg;
 import org.mortar.common.msg.Explosion;
 import org.mortar.common.msg.MortarMessage;
 import org.mortar.common.msg.Prepare;
+import org.mortar.sensor.msg.ReceivedMessage;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
@@ -20,14 +23,16 @@ public class SerializationTests {
 	private static final int PDU_SIZE = 134;
 
 	@Test
-	public void test() throws IOException {
+	public void test() throws Exception {
 		MessageSerializer serializer = new MessageSerializer(MortarMessage.class.getPackage());
+		MessageSerializer deserializer = new MessageSerializer(ReceivedMessage.class.getPackage());
 		MortarMessage[] msgs = { new ConfigMsg(Robolectric.application), new Explosion(), new Prepare() };
 		for (MortarMessage msg : msgs) {
 			byte[] serialized = serializer.serialize(msg);			
 			int length = serialized.length;
 			System.out.println(msg.getClass().getSimpleName()+":"+length);
 			assertThat(length, lessThanOrEqualTo(PDU_SIZE));
+			assertThat(deserializer.deserialize(serialized), instanceOf(ReceivedMessage.class));			
 		}
 	}
 }
